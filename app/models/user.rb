733d75573246d377c2891c90c4aacfa006be4d50
Tagaki Aki <a-tagaki@esm.co.jp>
2012-04-18 15:27:47 +0900

    kaminariを入れてページングできるようにした

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   has_secure_password
   before_save :create_remember_token
 
+  paginates_per 30
   validates :name, presence: true, length:{ maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
