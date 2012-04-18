febddc1f57f885daf83958b80488a3e1346ba4ce
Tagaki Aki <a-tagaki@esm.co.jp>
2012-04-18 12:23:18 +0900

    indexページを作った

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
   before_filter :signed_in_user, only: [:edit, :update]
+  before_filter :correct_user, only:[:edit, :update]
+
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
@@ -24,7 +30,6 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
@@ -41,6 +46,14 @@ class UsersController < ApplicationController
   private
 
   def signed_in_user
-    redirect_to signin_path, notice: "Please sign in." unless signed_in?
+    unless signed_in?
+      store_location
+      redirect_to signin_path, notice: "Please sign in."
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user)
   end
 end
