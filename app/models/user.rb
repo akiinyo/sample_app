83935b0c59e0adc9f4d79643b575f8f4d7b9b8d3
Tagaki Aki <a-tagaki@esm.co.jp>
2012-04-19 16:21:29 +0900

    コメントをポストできるようにした

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password
+  has_many :microposts, dependent: :destroy
   before_save :create_remember_token
 
   paginates_per 30
@@ -9,6 +10,10 @@ class User < ActiveRecord::Base
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6 }
   validates :password_confirmation, presence: true
+
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
 
   private
 
